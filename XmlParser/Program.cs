3672e205e25c8e84c53ea56982cc44e40a6c3253
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-01-18 11:51:19 +0000

    Add command-line args support

--- XmlParser/Program.cs
+++ XmlParser/Program.cs
@@ -1,6 +1,7 @@
 ﻿#nullable enable
 using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace XmlParser
 {
@@ -8,13 +9,25 @@ namespace XmlParser
     {
         private static void Main(string[] args)
         {
-            var sw = Stopwatch.StartNew();
-            XmlParser.Parse(Samples.struct_svg_03_f.AsSpan());
-            //XmlTextReaderParser.Parse(Samples.struct_svg_03_f);
-            //XmlParser.Parse(Samples.Empty.AsSpan());
-            //XmlTextReaderParser.Parse(Samples.Empty);
-            sw.Stop();
-            Console.WriteLine($"{sw.Elapsed.TotalMilliseconds}ms");
+            if (args.Length == 1)
+            {
+                var path = args[0];
+                var svg = File.ReadAllText(path);
+                var sw = Stopwatch.StartNew();
+                XmlParser.Parse(svg.AsSpan());
+                //XmlTextReaderParser.Parse(svg);
+                sw.Stop();
+                Console.WriteLine($"{sw.Elapsed.TotalMilliseconds}ms");
+            }
+            else
+            {
+                var sw = Stopwatch.StartNew();
+                XmlParser.Parse(Samples.struct_svg_03_f.AsSpan());
+                //XmlTextReaderParser.Parse(Samples.struct_svg_03_f);
+                //XmlParser.Parse(Samples.Empty.AsSpan());
+                //XmlTextReaderParser.Parse(Samples.Empty);
+                Console.WriteLine($"{sw.Elapsed.TotalMilliseconds}ms");
+            }
         }
     }
 }
