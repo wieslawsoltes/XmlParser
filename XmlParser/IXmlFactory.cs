09a9111d59bdce65c069f8bd45f7349ac61ada97
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-01-24 20:47:13 +0100

    Update IXmlFactory.cs

--- XmlParser/IXmlFactory.cs
+++ XmlParser/IXmlFactory.cs
@@ -5,9 +5,11 @@ namespace XmlParser
 {
     public interface IXmlFactory
     {
+        void PushElement();
         void PushElement(ReadOnlySpan<char> elementName);
-        void PushElement(string elementName);
+        void PushElement(string? elementName);
         void PopElement();
+        void SetElementName(ReadOnlySpan<char> elementName);
         void AddElementContent(ReadOnlySpan<char> content);
         void AddElementContent(string content);
         void AddElementAttribute(ReadOnlySpan<char> key, ReadOnlySpan<char> value);
